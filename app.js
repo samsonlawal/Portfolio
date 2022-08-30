68adb1bc70c55407753dcf16a30cf7b3b1ea1c5b
Samson <lawal4211@gmail.com>
2022-08-30 06:57:45 +0100

    Added scroll up arrow function

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
+// Change menu icon onClick for small screen
 function myFunction(x) {
   x.classList.toggle("fa-times");
 }
 
-function changebg() {
-  var navbar = document.querySelector("#navbar");
+// Nav background change on scroll
+// function changebg() {
+//   var navbar = document.querySelector("#navbar");
+//   var scrollvalue = window.scrollY;
+//   if (scrollvalue >= 200) {
+//     navbar.classList.add("bgColor");
+//   } else {
+//     navbar.classList.remove("bgColor");
+//   }
+// }
+
+// window.addEventListener("scroll", changebg);
+
+// Back to top
+
+const arrowUp = document.querySelector(".arrow-up");
+
+arrowUp.addEventListener("click", function () {
+  window.scrollTo(0, 0);
+});
+
+function arrowAppear() {
+  const arrowUp = document.querySelector(".arrow-up");
   var scrollvalue = window.scrollY;
   if (scrollvalue >= 200) {
-    navbar.classList.add("bgColor");
+    arrowUp.classList.add("arrow-up-appear");
   } else {
-    navbar.classList.remove("bgColor");
+    arrowUp.classList.remove("arrow-up-appear");
   }
 }
 
-window.addEventListener("scroll", changebg);
+window.addEventListener("scroll", arrowAppear);
+
+ScrollReveal().reveal("#About");
